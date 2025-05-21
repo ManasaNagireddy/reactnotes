e02027d8729f1f738246da4c6cbfb44f1ab398b4
ManasaNagireddy <manasa.bandapalli@gmail.com>
2025-05-21 23:14:24 +0530

    Learn how to use React package managers

--- App.js
+++ App.js
@@ -6,8 +6,16 @@
 	</div>
 </div>
 ****/
-var parenthtml = React.createElement("div",{id:"section1"},React.createElement("div",{id:"child"},[React.createElement("h1",{},"Hello"),React.createElement("h1",{},"World")]));
-var heading = React.createElement("h1",{"id":"heading1","xyz":"Namasate"},"Hello basic react example friend!!");
+import React from "react";
+import ReactDOM from "react-dom/client";
+var parenthtml = React.createElement("div",{id:"section1",key:"1"},React.createElement("div",{id:"child",key:2},
+	[
+		React.createElement("h1",{key:3},"Hello"),
+		React.createElement("h1",{key:6},"World")
+
+	])
+);
+var heading = React.createElement("h1",{"id":"heading1",key:"Namasate"},"Hello basic react example friend!!");
 console.log(parenthtml);
 var root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parenthtml);
